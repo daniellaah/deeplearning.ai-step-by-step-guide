7fc804e62d0f0203faefe2eef0fa6beb7a52128f
daniellaah <bobogao223@163.com>
2017-09-11 01:09:27 +0800

    update

--- 02-Improving-Deep-Neural-Networks/week1/main.py
+++ 02-Improving-Deep-Neural-Networks/week1/main.py
@@ -1,6 +1,8 @@
-from utils import load_dataset
-from DeepNeuralNetwork import DeepNeuralNetwork
 import numpy as np
+import matplotlib.pyplot as plt
+from utils import load_dataset, plot_decision_boundary
+from DeepNeuralNetwork import DeepNeuralNetwork
+
 
 if __name__ == '__main__':
     # ----------------------------load data-------------------------
@@ -9,26 +11,49 @@ if __name__ == '__main__':
     layers_dims = [train_X.shape[0], 10, 5, 1]
     activations = ['relu', 'relu', 'sigmoid']
     np.seterr(all='ignore')
+
     # --------------------------zero init----------------------------
-    dnn = DeepNeuralNetwork(layers_dims, activations, init='zero')
-    dnn = dnn.fit(train_X, train_Y, 15000, 0.01, print_cost=False, print_num=1000)
-    predictions_train = dnn.accuracy_score(train_X, train_Y)
+    dnn1 = DeepNeuralNetwork(layers_dims, activations, init='zero')
+    dnn1 = dnn1.fit(train_X, train_Y, 15000, 0.01, print_cost=False, print_num=1000)
+    predictions_train = dnn1.accuracy_score(train_X, train_Y)
     print ("zero init accuracy on train: {:.2f}".format(predictions_train))
-    predictions_test = dnn.accuracy_score(test_X, test_Y)
+    predictions_test = dnn1.accuracy_score(test_X, test_Y)
     print ("zero init accuracy on test: {:.2f}".format(predictions_test))
 
     # ------------------------large rand init-------------------------
-    dnn = DeepNeuralNetwork(layers_dims, activations, init='large')
-    dnn = dnn.fit(train_X, train_Y, 15000, 0.01, print_cost=False, print_num=1000)
-    predictions_train = dnn.accuracy_score(train_X, train_Y)
+    dnn2 = DeepNeuralNetwork(layers_dims, activations, init='large')
+    dnn2 = dnn2.fit(train_X, train_Y, 15000, 0.01, print_cost=False, print_num=1000)
+    predictions_train = dnn2.accuracy_score(train_X, train_Y)
     print ("large random init accuracy on train: {:.2f}".format(predictions_train))
-    predictions_test = dnn.accuracy_score(test_X, test_Y)
+    predictions_test = dnn2.accuracy_score(test_X, test_Y)
     print ("large random init accuracy on test: {:.2f}".format(predictions_test))
 
     # -----------------------------he init-----------------------------
-    dnn = DeepNeuralNetwork(layers_dims, activations, init='he')
-    dnn = dnn.fit(train_X, train_Y, 15000, 0.01, print_cost=False, print_num=1000)
-    predictions_train = dnn.accuracy_score(train_X, train_Y)
+    dnn3 = DeepNeuralNetwork(layers_dims, activations, init='he')
+    dnn3 = dnn3.fit(train_X, train_Y, 15000, 0.01, print_cost=False, print_num=1000)
+    predictions_train = dnn3.accuracy_score(train_X, train_Y)
     print ("he init accuracy on train: {:.2f}".format(predictions_train))
-    predictions_test = dnn.accuracy_score(test_X, test_Y)
+    predictions_test = dnn3.accuracy_score(test_X, test_Y)
     print ("he init accuracy on test: {:.2f}".format(predictions_test))
+
+
+    # plot decision boundary for zero initialization
+    plt.title("Model with zero initialization")
+    axes = plt.gca()
+    axes.set_xlim([-1.5,1.5])
+    axes.set_ylim([-1.5,1.5])
+    plot_decision_boundary(lambda x: dnn1.predict(x.T), train_X, train_Y)
+
+    # plot decision boundary for large random initialization
+    plt.title("Model with large random initialization")
+    axes = plt.gca()
+    axes.set_xlim([-1.5,1.5])
+    axes.set_ylim([-1.5,1.5])
+    plot_decision_boundary(lambda x: dnn2.predict(x.T), train_X, train_Y)
+
+    # plot decision boundary for he initialization
+    plt.title("Model with he initialization")
+    axes = plt.gca()
+    axes.set_xlim([-1.5,1.5])
+    axes.set_ylim([-1.5,1.5])
+    plot_decision_boundary(lambda x: dnn3.predict(x.T), train_X, train_Y)
